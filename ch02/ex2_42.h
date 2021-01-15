6b07a1183d6399c38c144cac80c68c8011e02349
Heng Yi <52519593+hengyi111@users.noreply.github.com>
2021-01-15 20:26:00 +0800

    Add files via upload

--- ch02/ex2_42.h
+++ ch02/ex2_42.h
@@ -1,3 +1,6 @@
+#ifndef SALES_DATA_H_INCLUDED
+#define SALES_DATA_H_INCLUDED
+
 #include <iostream>
 #include <string>
 
@@ -86,67 +89,4 @@ ostream &operator<<(ostream &out, const Sales_data &s)
 	return out;
 }
 
-int main()
-{
-	Sales_data book;
-	cout << "请输入销售记录：" << endl;
-	while (cin >> book)
-	{
-		cout << "ISBN、售出本数、原始价格、实际价格、折扣为" << book << endl;
-	}
-	Sales_data trans1, trans2;                                                                            
-	cout << "请输入两条ISBN相同的销售记录：" << endl;
-	cin >> trans1 >> trans2;
-	if (compareIsbn(trans1, trans2))
-		cout << "请输入两条ISBN相同的销售记录：" << endl;
-	cin >> trans1 >> trans2;
-	if (compareIsbn(trans1, trans2))
-		cout << "汇总数量：ISBN、售出件数、实际价格、折扣为 " << trans1 + trans2 << endl;
-	else
-		cout << "两条销售记录的ISBN不同" << endl;
-	Sales_data total, trans;
-	cout << "请输入几条ISBN相同的销售记录：" << endl;
-	if (cin >> total)
-	{
-		while (cin >> trans)
-		{
-			if (compareIsbn(total, trans))
-				total = total + trans;
-			else
-			{
-				cout << "当前书籍ISBN不同" << endl;
-				break;
-			}
-		}
-		cout << "有效汇总信息：ISBN、售出件数、原始价格、实际价格、折扣为 " << total << endl;
-	}
-	else
-	{
-		cout << "没有数据" << endl;
-		return -1;
-	}
-
-	int num = 1;
-	cout << "请输入若干销售记录：" << endl;
-	if (cin >> trans1)
-	{
-		while (cin >> trans2)
-		{
-			if (compareIsbn(trans1, trans2))
-				num++;
-			else
-			{
-				cout << trans1.isbn() << "共有" << num << "条销售记录" << endl;
-				trans1 = trans2;
-				num = 1;
-			}
-		}
-		cout << trans1.isbn() << "共有" << num << "条销售记录" << endl;
-	}
-	else
-	{
-		cout << "没有数据" << endl;
-		return -1;
-	}
-	return 0;
-}
+#endif  // SALES_DATA_H_INCLUDED
